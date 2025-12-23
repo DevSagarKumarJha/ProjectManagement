a1d0a6035e857d8d0a00c6f7f175a4ba6562714f
devsagarkumarjha <devsagarkumarjha@gmail.com>
2025-12-23 19:17:22 +0530

    feat (backend) add user model to the code base

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors"
+
+
+
+const app = express();
+
+// Basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configuration
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(", ") || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+import healthCheckRouter  from "./routes/healthcheck.routes.js";
+
+// import  routes
+app.use("/api/v1/health-check/", healthCheckRouter)
+app.get("/", (req, res) => {
+    res.send("Base camp");
+});
+
+export default app;
---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,25 @@
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const healthCheck = asyncHandler(async(req,res)=>{
+    res.status(200).json(
+        new ApiResponse(200, { message: "Server is running" }),
+    );
+})
+
+/**
+ * 
+const healthCheck =async (req, res) => {
+    const user = await getUser();
+    try {
+        res.status(200).json(
+            
+        );
+    } catch (error) {
+        next(error)
+    }
+};
+*/
+
+
+export {healthCheck};
